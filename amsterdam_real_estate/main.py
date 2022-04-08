085d9fcda1f763f8b6042350effd4accab749fd9
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-04-08 12:46:35 -0300

    Update main.py

--- amsterdam_real_estate/main.py
+++ amsterdam_real_estate/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('data_set/HousingPrices-Amsterdam-August-2021.csv')
 print(data.columns)
@@ -14,6 +16,17 @@ mean = int(round(data['Price'].mean(), 0))
 
 print(f'Com um preço médio de {mean} euros')
 
+
+data_map = data[['Address', 'Lat', 'Lon', 'Price', 'Room', 'Area']]
+map = px.scatter_mapbox(data_map, lat='Lat', lon='Lon',\
+                        hover_name='Address' , hover_data=['Price', 'Room', 'Area'],\
+                        color_discrete_sequence = ['blue'], zoom=10)
+
+map.update_layout(mapbox_style='open-street-map')
+map.update_layout( height=600, margin={'r':0, 't':0, 'l':0, 'b':0})
+map.show()
+
+
 x = int(input('Digite um número de quartos: '))
 
 data_x = data[data['Room'] == x]
@@ -22,11 +35,19 @@ mean_x = int(round(data_x['Price'].mean()))
 
 print(f'Existem {num_x} casas com {x} quartos, com um preço médio de {mean_x} euros')
 
+
 data_map = data_x[['Address', 'Lat', 'Lon', 'Price', 'Room', 'Area']]
-map = px.scatter_mapbox( data_map, lat='Lat', lon='Lon',\
+map_x = px.scatter_mapbox( data_map, lat='Lat', lon='Lon',\
                         hover_name='Address' , hover_data=['Price', 'Room', 'Area'],\
                         color_discrete_sequence = ['fuchsia'], zoom=10)
 
-map.update_layout(mapbox_style='open-street-map')
-map.update_layout( height=600, margin={'r':0, 't':0, 'l':0, 'b':0})
-map.show()
+map_x.update_layout(mapbox_style='open-street-map')
+map_x.update_layout( height=600, margin={'r':0, 't':0, 'l':0, 'b':0})
+map_x.show()
+
+
+plt.rcParams['figure.figsize'] = (10,8)
+sns.set_theme(style="darkgrid")
+
+sns.relplot(x="Area", y="Price", hue="Room", data=data)
+plt.show()
